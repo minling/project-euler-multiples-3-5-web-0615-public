549576ab497317a934df25173625b323d3386738
minling <mzhao725@gmail.com>
2015-07-01 11:54:03 -0400

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,17 @@
 # Enter your procedural solution here!
+require 'pry'
+
+  def collect_multiples(int) 
+    numbers = (1...int).to_a
+    numbers.collect do |number|
+      if (number % 3 == 0) || (number % 5 ==0)
+        number
+      end
+    end.compact
+  end
+
+  def sum_multiples(int)
+    collect_multiples(int).inject do |sum, number|
+      sum + number
+    end
+  end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :int
+  def initialize(int)
+    @int = int
+  end
+
+  def collect_multiples
+    numbers = (1...@int).to_a
+
+    numbers.collect do |number|
+      if (number % 3 == 0) || (number % 5 ==0)
+        number
+      end
+    end.compact
+  end
+
+  def sum_multiples
+    collect_multiples.inject do |sum, number|
+      sum + number
+    end
+  end
+
+end
